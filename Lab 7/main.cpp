25a6c0f694ac6a6a1eaafcc73cf4b37f807b2002
Caynereed <reed146@tritons.iowacentral.edu>
2023-12-20 21:38:49 -0600

    Create main.cpp

---
+++ Lab 7/main.cpp
@@ -0,0 +1,24 @@
+// Main
+// ~~~~~~~~~~~~~
+// NOTE
+// I was making final changes in my code when switching around my cout functions when I got a popup saying "fix in ostream", mistakenly I pressed it. So now the code won't run and I tried copying and pasting into a new file and it wouldn't work there. So I think it is just a problem with my xcode system, or clientside, but I do not have time to redownload it as people on Online forums suggest. Sorry if the code does not run but the last time I ran it I had it close to working with two fully functional boards. Just didn't get the loop correctly finished. 
+// 
+// ~~~~~~~~~~~~
+
+#include "Battleship.hpp"
+
+// Calling Functions
+void PlayGame();
+
+// Run Program
+int main()
+{
+    PlayGame();
+    
+    BattleshipBoard board;
+    
+    board.Instructions();
+    
+    board.playGame();
+    return 0;
+}
