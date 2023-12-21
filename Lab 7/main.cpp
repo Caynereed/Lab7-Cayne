84a7262e0cdd1f8ecf4854660c5656b598345659
Caynereed <reed146@tritons.iowacentral.edu>
2023-12-20 21:40:40 -0600

    Update main.cpp

--- Lab 7/main.cpp
+++ Lab 7/main.cpp
@@ -1,7 +1,13 @@
 // Main
 // ~~~~~~~~~~~~~
 // NOTE
-// I was making final changes in my code when switching around my cout functions when I got a popup saying "fix in ostream", mistakenly I pressed it. So now the code won't run and I tried copying and pasting into a new file and it wouldn't work there. So I think it is just a problem with my xcode system, or clientside, but I do not have time to redownload it as people on Online forums suggest. Sorry if the code does not run but the last time I ran it I had it close to working with two fully functional boards. Just didn't get the loop correctly finished. 
+// I was making final changes in my code when switching around my cout functions
+// when I got a popup saying "fix in ostream", mistakenly I pressed it. So now
+// the code won't run and I tried copying and pasting into a new file and it wouldn't
+// work there. So I think it is just a problem with my xcode system, or clientside,
+// however I do not have time to redownload it as people on Online forums suggest.
+// Sorry if the code does not run but the last time I ran it I had it close to working
+// with two fully functional boards. Just didn't get the loop correctly finished.
 // 
 // ~~~~~~~~~~~~
 
